eb3f60906af224ba15c0313f77ae301b5b51ff91
Matteo Di Sante <matteo.disante@live.it>
2024-10-07 16:55:40 +0200

    minor changes

--- 4th assignment/assignment_4.py
+++ 4th assignment/assignment_4.py
@@ -2,34 +2,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.interpolate import InterpolatedUnivariateSpline
 
-"""
-class PDF:
-
-    def __init__(self, x, y):
-        self._x = x
-        self._y = y
-        self._spline = InterpolatedUnivariateSpline(x, y) #_spline con _ perchè non volgio esporla ma tenerla privata
 
 
-#    def evaluate(self, x):
-#        return self._spline(x)
 
-    def __call__(self, x):  # Rende l'oggetto _spline chiamabile. __call_ è un metodo speciale
-        return self._spline(x)
+# class PDF:
+#
+#     def __init__(self, x, y):
+#         self._x = x
+#         self._y = y
+#         self._spline = InterpolatedUnivariateSpline(x, y) #_spline con _ perchè non volgio esporla ma tenerla privata
+#
+#
+# #    def evaluate(self, x):
+# #        return self._spline(x)
+#
+#     def __call__(self, x):  # Rende l'oggetto _spline chiamabile. __call_ è un metodo speciale
+#         return self._spline(x)
+#
+#
+#     def plot(self):
+#         plt.plot(self._x, self._y, 'o')
+#         x = np.linspace(self._x.min(), self._x.max(), 250)
+#         plt.plot(x, self(x))
+#
+#
+#     def integral(self, x1, x2):
+#         return self._spline.integral(x1, x2)
+#
+#     def normalization(self):
+#         return self.integral(self._x.min(), self._x.max())
 
 
-    def plot(self):
-        plt.plot(self._x, self._y, 'o')
-        x = np.linspace(self._x.min(), self._x.max(), 250)
-        plt.plot(x, self(x))
 
-
-    def integral(self, x1, x2):
-        return self._spline.integral(x1, x2)
-
-    def normalization(self):
-        return self.integral(self._x.min(), self._x.max())
-"""
 
 
 # Col codice di di seguito EREDITO i metodi dalla classe Spline senza riscrivere tutti i metodi come fatto sopra
@@ -53,7 +57,9 @@ class PDF(InterpolatedUnivariateSpline):
         return self.integral(self._x.min(), self._x.max())
 
 
-### WARNING: Nonostante la classe PDF sia ambiata NOTEVOLEMNTE la parte dell' " if __name__ == '__main__': " NON è CAMBIATA!!!!!!
+### WARNING: Nonostante la classe PDF sia cambiata NOTEVOLEMNTE rispetto alla sezione commentata sorpa,
+###          la parte del "" if __name__ == '__main__': " NON è CAMBIATA!!!!!!
+### Questo perchè si sono MANTENUTE LE STESSE INTERFACCE
 
 
 # Il codice qui sotto è come la gente usa il codice. Si parte dalle interfacce
@@ -61,7 +67,7 @@ if __name__ == '__main__':
     x = np.linspace(0., 1., 4)
     y = np.exp(x)
     pdf = PDF(x,y) #pdf è un'istanza della classe PDF
-    print(id(pdf))
+    print(id(pdf)) # id(pdf) dà l’indirizzo di memoria dell’istanza della classe PDF (l’oggetto pdf).
     x0 = 0.5
     print(np.exp(x0), pdf(x0))
     print(pdf.integral(0., 1.))
